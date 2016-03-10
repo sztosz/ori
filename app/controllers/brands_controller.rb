a5776606516f9c00807078d5950dcd1acbe830f3
Bartosz Nowak <sztosz@gmail.com>
2016-03-10 10:41:32 +0100

    Added Brands

---
+++ app/controllers/brands_controller.rb
@@ -0,0 +1,51 @@
+class BrandsController < ApplicationController
+  def index
+    @brands = Brand.all
+  end
+
+  def show
+    @brand = Brand.find params[:id]
+  end
+
+  def new
+    @brand = Brand.new
+    authorize @brand
+  end
+
+  def create
+    @brand = Brand.new brand_params
+    # raise ActiveRecord::RecordInvalid 'bla'
+    authorize @brand
+    if @brand.save!
+      redirect_to action: 'index'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @brand = Brand.find params[:id]
+    authorize @brand
+  end
+
+  def update
+    @brand = Brand.find params[:id]
+    authorize @brand
+    if @brand.update brand_params
+      redirect_to action: 'index'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @brand = Brand.find params[:id]
+    authorize @brand
+    @brand.destroy
+    redirect_to brands_path
+  end
+
+  def brand_params
+    params.require(:brand).permit(:name)
+  end
+end
---
+++ app/policies/brand_policy.rb
@@ -0,0 +1,21 @@
+class BrandPolicy < ApplicationPolicy
+  def new?
+    create?
+  end
+
+  def create?
+    @user.admin?
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    @user.admin?
+  end
+
+  def destroy?
+    @user.admin?
+  end
+end
